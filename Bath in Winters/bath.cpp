f8aaa5d1c7d0ced1b1b4810837ab09a2fdecfb20
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2022-10-30 13:22:30 +0530

    Create bath.cpp

---
+++ Bath in Winters/bath.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y;
+	    cin >> x >> y;
+	    
+	    cout << x/(2*y) << endl;
+	}
+	return 0;
+}
