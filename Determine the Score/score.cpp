5c0242dba90360369894a8cdaa890e123bac1b8e
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2022-10-30 13:25:48 +0530

    Create score.cpp

---
+++ Determine the Score/score.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,n,a,b;
+	    cin>>x>>n;
+	    a=x/10;
+	    b=a*n;
+	    cout<<b<<endl;
+	}
+	return 0;
+}
