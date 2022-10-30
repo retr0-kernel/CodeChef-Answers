b96c5c57f7fe44e383f4afb4a7da53848b016070
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2022-10-30 13:19:05 +0530

    Create sd.cpp

---
+++ Sleep Deprivation/sd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	     if(n<7){
+	         cout << "Yes" << endl;
+	     }
+	     else{
+	         cout << "No" << endl;
+	     }
+	}
+	return 0;
+}
