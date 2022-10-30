7846a121ea258b3ae2c7cda8593bfb8751ebfdbd
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2022-10-30 13:27:14 +0530

    Create audible.cpp

---
+++ Audible/audible.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int X;
+	    cin>>X;
+	    if(X>=67 && X<=45000)
+	    cout<<"YES"<<endl;
+	    
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
