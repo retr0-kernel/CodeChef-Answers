95ba3889585c1a7e4b6025dce5cc2180325217f6
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2022-10-30 13:24:39 +0530

    Create burgers.cpp

---
+++ Burgers/burgers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<y<<endl;
+	    }
+	    else
+	    cout<<x<<endl;
+	}
+	return 0;
+}
